61928c95abef918d50b3455f371411ca81c0e1f7
ryan <ryan@wavetogether.com>
2021-05-05 15:07:41 +0900

    change logger

--- src/Logger.ts
+++ src/Logger.ts
@@ -4,23 +4,23 @@ const APP_NAME = 'mediasoup-client';
 
 export class Logger
 {
-	private readonly _debug: debug.Debugger;
-	private readonly _warn: debug.Debugger;
-	private readonly _error: debug.Debugger;
+	private readonly _debug: string;
+	private readonly _warn: string;
+	private readonly _error: string;
 
 	constructor(prefix?: string)
 	{
 		if (prefix)
 		{
-			this._debug = debug(`${APP_NAME}:${prefix}`);
-			this._warn = debug(`${APP_NAME}:WARN:${prefix}`);
-			this._error = debug(`${APP_NAME}:ERROR:${prefix}`);
+			this._debug = `${APP_NAME}:${prefix}`;
+			this._warn = `${APP_NAME}:WARN:${prefix}`;
+			this._error = `${APP_NAME}:ERROR:${prefix}`;
 		}
 		else
 		{
-			this._debug = debug(APP_NAME);
-			this._warn = debug(`${APP_NAME}:WARN`);
-			this._error = debug(`${APP_NAME}:ERROR`);
+			this._debug = APP_NAME;
+			this._warn = `${APP_NAME}:WARN`;
+			this._error = `${APP_NAME}:ERROR`;
 		}
 
 		/* eslint-disable no-console */
@@ -30,21 +30,15 @@ export class Logger
 		/* eslint-enable no-console */
 	}
 
-	get debug(): debug.Debugger
-	{
-		this._debug.enable('*');
-		return this._debug;
+	debug = (data: any) => {
+		console.info(`${this._debug} :`, data);
 	}
 
-	get warn(): debug.Debugger
-	{
-		this._warn.enable('*');
-		return this._warn;
+	warn = (data: any) => {
+		console.warn(`${this._warn} :`, data);
 	}
 
-	get error(): debug.Debugger
-	{
-		this._error.enable('*');
-		return this._error;
+	error = (data: any) => {
+		console.error(`${this._warn} :`, data);
 	}
 }
